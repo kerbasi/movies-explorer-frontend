eba370998bf522dce100d318443f721a29746a65
Kirill <kirill.novikov@gmial.com>
2023-07-25 22:59:37 +0300

    Add promo

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import "./Main.css";
 import Header from "../Header/Header";
+import Promo from "../Promo/Promo";
 
 function Main() {
   return (
     <div className='main'>
       <Header />
+      <Promo />
     </div>
   );
 }
---
+++ src/components/Promo/Promo.jsx
@@ -0,0 +1,17 @@
+﻿import "./Promo.css";
+import landingLogo from "../../images/pic__COLOR_landing-logo.svg";
+
+function Promo() {
+  return (
+    <section className='promo'>
+      <div className='promo__wrapper'>
+        {/* <img className='promo__logo' src={landingLogo} alt='logo' /> */}
+        <h1 className='promo__title'>
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export default Promo;
