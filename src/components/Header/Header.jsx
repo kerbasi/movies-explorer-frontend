4cc89356bb8a3cd42ed04a939db2fc658afb2897
Kirill <kirill.novikov@gmial.com>
2023-07-29 12:29:44 +0300

    Add navigation comp

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,83 +1,16 @@
 import "./Header.css";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../Logo/Logo";
-import accountLogo from "../../images/account.svg";
-import { useState } from "react";
+import Navigation from "../Navigation/Navigation";
 
 function Header() {
-  const [logined, setLogined] = useState(true);
   const location = useLocation().pathname;
   const className = location === "/" ? "header" : "header header_type_white";
   return (
     <header className={className}>
       <div className='header__wrapper'>
         <Logo />
-        {(location === "/movies" || location === "/saved-movies") && (
-          <div className='header__links-wrapper'>
-            <Link
-              className={`header__link ${
-                location === "/movies" ? "header__link_active" : ""
-              }`}
-              to='/movies'
-            >
-              Фильмы
-            </Link>
-            <Link
-              className={`header__link ${
-                location === "/saved-movies" ? "header__link_active" : ""
-              }`}
-              to='/saved-movies'
-            >
-              Сохранённые фильмы
-            </Link>
-          </div>
-        )}
-        {location === "/" && logined && (
-          <div className='header__links-wrapper header__links-wrapper_type_main'>
-            <Link className='header__link' to='/movies'>
-              Фильмы
-            </Link>
-            <Link className='header__link' to='/saved-movies'>
-              Сохранённые фильмы
-            </Link>
-          </div>
-        )}
-        {logined && (
-          <>
-            <Link
-              className={`header__link ${
-                location === "/"
-                  ? " header__link_type_profile header__link_type_profile-main"
-                  : "header__link_type_profile"
-              }`}
-              to='/profile'
-            >
-              {location === "/" ? (
-                <img
-                  className='header__account-logo'
-                  src={accountLogo}
-                  alt='account'
-                />
-              ) : (
-                ""
-              )}
-              Аккаунт
-            </Link>
-          </>
-        )}
-        {!logined && (
-          <>
-            <Link
-              className='header__link header__link_type_signup'
-              to='/signup'
-            >
-              Регистрация
-            </Link>
-            <Link className='header__link header__link_type_login' to='/signin'>
-              Войти
-            </Link>
-          </>
-        )}
+        <Navigation />
       </div>
     </header>
   );
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,85 @@
+import "./Navigation.css";
+import accountLogo from "../../images/account.svg";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+
+function Navigation() {
+  const [logined, setLogined] = useState(true);
+  const location = useLocation().pathname;
+
+  return (
+    <nav className='navigation'>
+      {(location === "/movies" || location === "/saved-movies") && (
+        <div className='navigation__links-wrapper'>
+          <Link
+            className={`navigation__link ${
+              location === "/movies" ? "navigation__link_active" : ""
+            }`}
+            to='/movies'
+          >
+            Фильмы
+          </Link>
+          <Link
+            className={`navigation__link ${
+              location === "/saved-movies" ? "navigation__link_active" : ""
+            }`}
+            to='/saved-movies'
+          >
+            Сохранённые фильмы
+          </Link>
+        </div>
+      )}
+      {location === "/" && logined && (
+        <div className='navigation__links-wrapper navigation__links-wrapper_type_main'>
+          <Link className='navigation__link' to='/movies'>
+            Фильмы
+          </Link>
+          <Link className='navigation__link' to='/saved-movies'>
+            Сохранённые фильмы
+          </Link>
+        </div>
+      )}
+      {logined && (
+        <>
+          <Link
+            className={`navigation__link ${
+              location === "/"
+                ? " navigation__link_type_profile navigation__link_type_profile-main"
+                : "navigation__link_type_profile"
+            }`}
+            to='/profile'
+          >
+            {location === "/" ? (
+              <img
+                className='navigation__account-logo'
+                src={accountLogo}
+                alt='account'
+              />
+            ) : (
+              ""
+            )}
+            Аккаунт
+          </Link>
+        </>
+      )}
+      {!logined && (
+        <>
+          <Link
+            className='navigation__link navigation__link_type_signup'
+            to='/signup'
+          >
+            Регистрация
+          </Link>
+          <Link
+            className='navigation__link navigation__link_type_login'
+            to='/signin'
+          >
+            Войти
+          </Link>
+        </>
+      )}
+    </nav>
+  );
+}
+
+export default Navigation;
