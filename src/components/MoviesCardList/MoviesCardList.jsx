a5810ccdc16d7ed41cb7fe988590777e76eae66f
Kirill <kirill.novikov@gmial.com>
2023-07-30 09:50:15 +0300

    Add adaptive for 768 for movies and saved

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 function MoviesCardList({ movies }) {
-  const [pagesLimit, setPagesLimit] = useState(16);
+  const [pagesLimit, setPagesLimit] = useState(8);
   const [slicedMovies, setSlicedMovies] = useState(movies.slice(0, pagesLimit));
   const paginationHandler = () => {
     setPagesLimit((prev) => prev + 16);
