80e40258b9c8401d8d20cbe4256815f96c028e23
Kirill <kirill.novikov@gmial.com>
2023-08-16 12:21:55 +0300

    Add error to search

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 import "./SearchForm.css";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import find from "../../images/find.svg";
 
 function SearchForm() {
+  const [inputText, setInputText] = useState("");
+  const [errorText, setErrorText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrorText(inputText ? "" : "Нужно ввести ключевое слово");
+  };
   return (
     <div className='search-form'>
       <form className='search-form__form'>
@@ -11,14 +19,18 @@ function SearchForm() {
             className='search-form__text-input'
             type='text'
             placeholder='Фильм'
+            onChange={(e) => setInputText(e.target.value)}
+            value={inputText || ""}
           />
           <button
             className='search-form__submit-button hover hover_type_button'
             type='submit'
+            onClick={handleSubmit}
           >
             <img className='search-form__find-img' src={find} alt='find' />
           </button>
         </div>
+        <div className='search-form__error'>{errorText}</div>
         <div className='search-form__filter-checkbox'>
           <FilterCheckbox label='Короткометражки' />
         </div>
